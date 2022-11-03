f45d57f64875c37fd38a357810b39628ebabcbb2
Kimeli <jobkimeli2016@gmail.com>
2022-11-03 08:20:18 +0300

    Added CustomAuthenticationProvider file

--- src/main/java/com/kimeli/oauthserver/config/DefaultSecurityConfig.java
+++ src/main/java/com/kimeli/oauthserver/config/DefaultSecurityConfig.java
@@ -1,7 +1,10 @@
 package com.kimeli.oauthserver.config;
 
+import com.kimeli.oauthserver.service.CustomAuthenticationProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -10,11 +13,17 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
 public class DefaultSecurityConfig {
+    @Autowired
+    private CustomAuthenticationProvider customAuthenticationProvider;
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
                 .formLogin(withDefaults());
         return httpSecurity.build();
+    }
+    @Autowired
+    public void bindAuthenticationProvider(AuthenticationManagerBuilder authenticationManagerBuilder){
+        authenticationManagerBuilder.authenticationProvider(customAuthenticationProvider);
     }
 
 }
